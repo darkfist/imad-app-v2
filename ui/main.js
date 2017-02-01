0e7b7c0da0f88e8a72a545b5a4cab447da6b5132
Nishant Ranjan <darkfist1998@gmail.com>
2017-02-02 02:36:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,25 @@
 // counter code
 var button = getElementById("counter");
-var counter = 0;
+
 button.onClick = function() {
     
+    // create a request object
+    var request = new XMLHttpRequest();
     
+    // capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if (request.readystate === XMLHttpRequest.DONE) {
+          // take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = getElementById("count");
+              span.innerHTML = counter.toString();
+          }
+      }  
+      // not done yet
+    };
     
-     counter = counter + 1;    
-     var span = getElementById("count");
-     span.innerHTML = counter.toString();
+    // make the request
+    request.open('GET', 'http://darkfist.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
