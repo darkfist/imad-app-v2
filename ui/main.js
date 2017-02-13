6512d49829d5bc32d4951690e437aa2e2f4696a4
Nishant Ranjan <darkfist1998@gmail.com>
2017-02-14 05:28:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 // counter code
-var button = document.getElementById("counter1");
+var button = document.getElementById("counter");
 
 button.onclick = function() {
     
@@ -12,7 +12,7 @@ button.onclick = function() {
           // take some action
           if (request.status === 200) {
               var counter = request.responseText;
-              var span = document.getElementById("count1");
+              var span = document.getElementById("count");
               span.innerHTML = counter.toString();
           }
       }  
