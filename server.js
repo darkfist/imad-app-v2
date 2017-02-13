2d739f9b0d2d3f645ced289e7062c136f74cf4e2
Nishant Ranjan <darkfist1998@gmail.com>
2017-02-14 05:13:02 +0530

    Made a profile page

--- server.js
+++ server.js
@@ -89,6 +89,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/mysite', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
+});
+
 var counter = 0;
 app.get('/counter', function (req, res) {
   counter = counter + 1;
@@ -113,12 +117,24 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/style2.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style2.css'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/home.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'home.jpg'));
+});
+
+app.get('/ui/nishant.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'nishant.jpg'));
 });
 
 
