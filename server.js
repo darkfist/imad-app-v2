2f5e1aa15e35c8a9adff675ef5ff9b8ee920e14f
Nishant Ranjan <darkfist1998@gmail.com>
2017-02-27 20:18:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var Pool = require('pg').Pool;
 var config = {
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate(data) {
     var title = data.title;
@@ -68,6 +70,21 @@ function hash (input, salt) {
 app.get('/hash/:input', function (req, res) {
    var hashedString = hash(req.params.input, 'this-isspmedfsd');
    res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+   var username =  req.body.username;
+   var password =  req.body.password;
+   
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbstring = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbstring], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }       
+   });
 });
 
 var pool = new Pool(config);
