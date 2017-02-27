b2aa89062bd4d90099ffd57beb5f1ec74ec9e5f7
Nishant Ranjan <darkfist1998@gmail.com>
2017-02-28 00:49:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var crypto = require('crypto');
-var bodyParser = require('body-parser');
 
 var config = {
     user: 'darkfist',
@@ -15,7 +13,6 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.json());
 
 function createTemplate(data) {
     var title = data.title;
@@ -62,54 +59,7 @@ app.get('/mysite', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash (input, salt) {
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
-}
 
-app.get('/hash/:input', function (req, res) {
-   var hashedString = hash(req.params.input, 'this-isspmedfsd');
-   res.send(hashedString);
-});
-
-app.post('/create-user', function (req, res) {
-   var username =  req.body.username;
-   var password =  req.body.password;
-   
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbstring = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbstring], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send('User successfully created: ' + username);
-      }       
-   });
-});
-
-app.post('/login', function (req, res) {
-   var username =  req.body.username;
-   var password =  req.body.password;
-   
-   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          if(result.rows.length===0) {
-              res.send(403).send('username/password is invalid');
-          } else {
-              var dbString = result.rows[0].password;
-              var salt = dbString.split('$')[2];
-              var hashedPassword = hash(password, salt);
-              if (hashedPassword === dbString) {
-                  res.send('Credentials are correct');
-              } else {
-                  res.send(403).send('username/password is invalid');
-              }
-          }
-      }       
-   });
-});
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
