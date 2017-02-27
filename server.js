4e3624a5de9275dea0a428412f2924a6df5ca620
Nishant Ranjan <darkfist1998@gmail.com>
2017-02-27 19:44:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/mysite', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash (input) {
+function hash (input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
