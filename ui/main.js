524c78d966349dd1e4d6c8db96c0d57bbce2207c
Nishant Ranjan <darkfist1998@gmail.com>
2017-02-28 01:57:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,4 @@
-// counter code
-var button = document.getElementById("counter");
-
-button.onclick = function() {
-    
-    // create a request object
-    var request = new XMLHttpRequest();
-    
-    // capture the response and store it in a variable
-    request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // take some action
-          if (request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById("count");
-              span.innerHTML = counter.toString();
-          }
-      }  
-      // not done yet
-    };
-    
-    // make the request
-    request.open('GET', 'http://darkfist.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-
-// submit name
+// submit username and password
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function() {
@@ -39,22 +12,21 @@ submit.onclick = function() {
           // take some action
           if (request.status === 200) {
               // capture a list of names and render it
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = "";
-              for (var i=0; i<names.length; i++) {
-                  list += '<li>' + names[i] + '</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;
+              console.log('success');
+              alert('success');
+          } else if (request.status === 403) {
+              alert('failed');
+          } else if (request.status === 500) {
+              alert('failed2');
           }
       }  
       // not done yet
     };
     
     // make the request
-    var nameInput = document.getElementById("name");
-    var name = nameInput.value;
-    request.open('GET', 'http://darkfist.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    request.open('POST', 'http://darkfist.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
